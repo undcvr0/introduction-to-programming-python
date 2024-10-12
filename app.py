dc59daf0d28d9de838d76b67fa2f513c5fb4a352
undcvr0 <diyor.iknu@gmail.com>
2024-10-12 20:50:44 +0500

    Request nickname name and last name

--- app.py
+++ app.py
@@ -1,1 +1,4 @@
 print('''I'm learning Git merge conflicts" to the console''')
+name = input('Please enter your name:')
+last_name = input('Please enter your last name:')
+print('Hi, {} {}!'.format(name, last_name))
